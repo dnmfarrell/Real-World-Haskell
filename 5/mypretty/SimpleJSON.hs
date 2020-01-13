814c2b821df3a417a60934ff47452c01d672f3d8
David Farrell <davidnmfarrell@gmail.com>
2020-01-13 13:29:18 -0500

    WIP json parser

--- 5/mypretty/SimpleJSON.hs
+++ 5/mypretty/SimpleJSON.hs
@@ -7,10 +7,15 @@ module SimpleJSON
   , getObject
   , getArray
   , isNull
+  , parseJSON
   ) where
 
+whitespace = "\n\t "
+atomStart = "\"tfn0123456789-["
+
 data JValue = JString String
-            | JNumber Double
+            | JDouble Double
+            | JInt Int
             | JBool Bool
             | JNull
             | JObject [(String, JValue)]
@@ -22,11 +27,11 @@ getString (JString s) = Just s
 getString _           = Nothing
 
 getInt :: JValue -> Maybe Int
-getInt (JNumber n) = Just (truncate n)
-getInt _           = Nothing
+getInt (JInt n) = Just n
+getInt _        = Nothing
 
 getDouble :: JValue -> Maybe Double
-getDouble (JNumber n) = Just n
+getDouble (JDouble n) = Just n
 getDouble _           = Nothing
 
 getBool :: JValue -> Maybe Bool
@@ -43,3 +48,102 @@ getArray _          = Nothing
 
 isNull :: JValue -> Bool
 isNull v = v == JNull
+
+parseJSON :: String -> JValue
+parseJSON [] = error "Cannot parse an empty string"
+parseJSON js = fst $ parseNext js "[{"
+
+parseNext :: String -> String -> (JValue, String)
+parseNext js allow
+  | not (any (x==) allow) = error $ "Expected one of: "++allow++" got: "++[x]
+  | otherwise = case x of
+  '"'  -> (parseJString ss, sr)
+  't'  -> (parseJBool vs, vr)
+  'f'  -> (parseJBool vs, vr)
+  'n'  -> (parseJNull vs, vr)
+  '0'  -> (parseJNumber vs, vr)
+  '1'  -> (parseJNumber vs, vr)
+  '2'  -> (parseJNumber vs, vr)
+  '3'  -> (parseJNumber vs, vr)
+  '4'  -> (parseJNumber vs, vr)
+  '5'  -> (parseJNumber vs, vr)
+  '6'  -> (parseJNumber vs, vr)
+  '7'  -> (parseJNumber vs, vr)
+  '8'  -> (parseJNumber vs, vr)
+  '9'  -> (parseJNumber vs, vr)
+  '-'  -> (parseJNumber vs, vr)
+  '['  -> (JArray (fst ar), snd ar)
+  '{'  -> (JObject (fst ob), snd ob)
+  _    -> error $ "Unrecognized character: " ++ [x]
+  where xs = skipWhitespace js
+        x  = head xs
+        t  = tail xs
+        vs = takeValue xs
+        vr = drop (length vs) xs
+        ss = takeString xs
+        sr = drop ((length ss) + 2) xs
+        ar = parseJArray ([], skipWhitespace t)
+        ob = parseJObject ([], skipWhitespace t)
+
+skipWhitespace:: String -> String
+skipWhitespace = drop =<< length . takeWhitespace
+
+parseJString :: String -> JValue
+parseJString js = JString js
+
+parseJBool :: String -> JValue
+parseJBool "true"  = JBool True
+parseJBool "false" = JBool False
+parseJBool _       = error "Expecting 'true' or 'false'"
+
+parseJNull :: String -> JValue
+parseJNull "null"  = JNull
+parseJNull _       = error "Expecting 'null'"
+
+parseJNumber :: String -> JValue
+parseJNumber js
+  | any ('.'==) js = JDouble $ (read js::Double)
+  | otherwise      = JInt $ (read js::Int)
+
+takeValue :: String -> String
+takeValue = takeWhile (\j -> not (any (j==) (whitespace ++ ",]}")))
+
+takeString :: String -> String
+takeString ('"':xs) = takeStrWhile xs
+  where takeStrWhile [] = error "Unterminated string"
+        takeStrWhile (x:xs)
+          | x == '"'  = []
+          | x == '\\' && not (null xs) = case head xs of
+                          '"' -> x: (head xs :(takeStrWhile (tail xs)))
+                          _   -> x: (takeStrWhile xs)
+          | otherwise = x: (takeStrWhile xs)
+
+takeWhitespace :: String -> String
+takeWhitespace = takeWhile (\j -> any (j==) whitespace)
+
+parseJArray :: ([JValue], String) -> ([JValue], String)
+parseJArray (xs, ']':js) = (xs, js)
+parseJArray (xs, ',':js) 
+  | any (n==) ",]"  = error $ "Expected an element but found: " ++ [n]
+  | otherwise = parseJArray (xs, ns)
+  where ns = skipWhitespace js
+        n  = head ns
+
+parseJArray (xs, js) = parseJArray (xs ++ [nv1], nv2)
+  where nv = parseNext js atomStart
+        nv1= fst nv
+        nv2= snd nv
+
+parseJObject :: ([(String, JValue)], String) -> ([(String, JValue)], String)
+parseJObject = undefined
+--parseJObject (xs, '}':js) = (xs, js)
+--parseJObject (xs, ',':js) 
+--  | n == ','  = error "Encountered ',' but expected an element"
+--  | otherwise = parseJObject (xs, ns)
+--  where ns = skipWhitespace js
+--        n  = head ns
+--
+--parseJObject (xs, js) = parseJObject (xs ++ [nv1], nv2)
+--  where nv = parseNext js atomStart
+--        nv1= fst nv
+--        nv2= snd nv
